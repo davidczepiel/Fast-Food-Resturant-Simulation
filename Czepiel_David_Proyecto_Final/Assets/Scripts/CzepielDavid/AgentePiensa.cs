b1204ae75ac8c69eb12fb7f9dfca9d51d646fea9
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2021-06-09 21:40:19 +0200

    Introducidas imagenes pensamientos

---
+++ Czepiel_David_Proyecto_Final/Assets/Scripts/CzepielDavid/AgentePiensa.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentePiensa : MonoBehaviour
+{
+    public GameObject camera;
+
+    public Sprite hamburguesa;
+    public Sprite patatas;
+    public Sprite helado;
+    public Sprite bebida;
+
+    public float tiempoMostrar = 2;
+    private float timer;
+
+    public enum Pensamiento { hamburguesa, patatas, bebida, helado };
+
+    public SpriteRenderer miImagen;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        timer = tiempoMostrar;
+        camera = GameObject.Find("Main Camera");
+        miImagen = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            miImagen.enabled = false;
+        }
+        this.transform.forward = (camera.transform.position - this.transform.position).normalized;
+
+        if (Input.GetKeyDown("space"))
+        {
+            mostrarImagen(Pensamiento.hamburguesa);
+        }
+    }
+
+    public void mostrarImagen(Pensamiento nuevo)
+    {
+        switch (nuevo)
+        {
+            case Pensamiento.hamburguesa:
+                timer = tiempoMostrar;
+                miImagen.enabled = true;
+                miImagen.sprite = hamburguesa;
+                break;
+
+            case Pensamiento.patatas:
+                timer = tiempoMostrar;
+                miImagen.enabled = true;
+                miImagen.sprite = patatas;
+                break;
+
+            case Pensamiento.bebida:
+                break;
+
+            case Pensamiento.helado:
+                break;
+        }
+    }
+}
